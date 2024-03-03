d1ad6fde644f92a67942bc4ee0b4b17f52f41f6b
riddhi <91315409+Riddhi12349@users.noreply.github.com>
2024-03-03 17:01:32 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Perfect Sum Problem/perfect-sum-problem.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+	public:
+	const int mod = 1e9+7;
+	
+	int find(int i , int sum , int a[] , int n, vector<vector<int>>& dp){
+	   
+	    if(i >= n){ return sum==0;}
+	    
+	    if(dp[i][sum] != -1) return dp[i][sum]%mod;
+	    
+	    int pick = 0;
+	    if(sum-a[i] >= 0)
+	      pick = find(i+1 , sum-a[i] , a , n, dp);
+	    
+	    int notpick = find(i+1 , sum , a, n , dp);
+	    
+	    return dp[i][sum] = (pick+notpick)%mod;
+	}
+	int perfectSum(int a[], int n, int sum)
+	{
+	    vector<vector<int>> dp(n+1 , vector<int>(sum+1 , -1));
+       return find(0 , sum , a , n , dp);
+	}
+	  
+};
+
+//{ Driver Code Starts.
+int main() 
+{
+   	
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, sum;
+
+        cin >> n >> sum;
+
+        int a[n];
+        for(int i = 0; i < n; i++)
+        	cin >> a[i];
+
+       
+
+	    Solution ob;
+	    cout << ob.perfectSum(a, n, sum) << "\n";
+	     
+    }
+    return 0;
+}
+
+// } Driver Code Ends
