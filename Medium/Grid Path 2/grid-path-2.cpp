3ec33843b574765d7d07ac2b9e2d6d0f6e3154f8
riddhi <91315409+Riddhi12349@users.noreply.github.com>
2024-03-04 12:53:57 +0530

    Updated Solution - GfG to GitHub

--- Medium/Grid Path 2/grid-path-2.cpp
+++ Medium/Grid Path 2/grid-path-2.cpp
@@ -5,26 +5,48 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-  int dp[105][105];
+  int dp[502][502];
   const int mod = 1e9+7;
-   int find(int i , int j , int m , int n , vector<vector<int>>& grid){
-       
-       if(i==m-1 && j==n-1 && grid[i][j]==0) return 1;
-       
-       if(i<0 || j<0 || i>= m || j>=n) return 0;
-       if(grid[i][j]==1) return 0;
-       if(dp[i][j] != -1) return dp[i][j];
-       
-      int p = find(i+1, j, m , n , grid);
-      int q = find(i , j+1 , m , n , grid);
-       
-      return dp[i][j] = (p+q)%mod;
-   }
-    int totalWays(int m, int n, vector<vector<int>>& grid) {
-     
-    //  vector<vector<int>> dp(m , vector<int>(n,0));
-      memset(dp , -1, sizeof(dp));
-      return find(0,0,m,n,grid);
+  
+    int find(int i,int j , int m, int n,vector<vector<int>>& grid){
+    
+        if(i==0 && j==0){
+             if(grid[i][j]==0) return 1;
+             return 0;
+        }
+        if(i<0 || j<0) return 0;
+        
+        if(grid[i][j]==1) return 0;
+        
+        if(dp[i][j] != -1) return dp[i][j];
+        
+        int p1 = find(i-1,j,m,n,grid);
+        int p2 = find(i,j-1,m,n,grid);
+        
+        return dp[i][j]=( p1+p2)%mod;
+    }
+    
+    int totalWays(int m,int n, vector<vector<int>>& grid) {
+        
+    //memset(dp , 0 ,sizeof(dp));
+      //return  find(m-1,n-1,m,n,grid);
+    int dp[m][n];
+    
+    for(int i = m-1 ; i >= 0; i--){
+        for(int j = n-1 ; j >= 0 ; j--){
+            
+            if(grid[i][j]==1) { dp[i][j]=0; continue; }
+            if(i==m-1 && j==n-1) { dp[i][j] = 1; continue; }
+          
+            int op1 = 0 , op2= 0;
+            if(i+1 < m) op1 = dp[i+1][j];
+            if(j+1 < n) op2 = dp[i][j+1];
+            
+            dp[i][j] = (op1+op2)%mod;
+        }
+    }
+    
+    return dp[0][0];
     }
 };
 
