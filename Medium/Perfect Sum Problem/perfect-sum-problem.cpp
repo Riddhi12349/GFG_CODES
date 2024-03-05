af7d14c1ffbca7bfd7fcadbeb487f67465362840
riddhi <91315409+Riddhi12349@users.noreply.github.com>
2024-03-05 23:38:53 +0530

    Updated Solution - GfG to GitHub

--- Medium/Perfect Sum Problem/perfect-sum-problem.cpp
+++ Medium/Perfect Sum Problem/perfect-sum-problem.cpp
@@ -28,13 +28,16 @@ class Solution{
        //return find(0 , sum , a , n , dp);
     //   dp[n][0]=1; 
     //   /
-    for(int i = 0 ; i <= n  ; i++)
-      dp[i][0] = 1;
+    
+    // for(int i = 0 ; i <= n  ; i++)
+    //   dp[i][0] = 1;
+      dp[n][0]=1;
       
        for(int i = n-1 ; i >= 0 ; i--){
            for(int j = 0 ;  j <= sum ; j++){
                 
-               
+            //  if(j==0){dp[i][j]=1;}
+             
                 int p = 0;
                 if(j-a[i] >= 0)
                     p = dp[i+1][j-a[i]];
@@ -42,6 +45,7 @@ class Solution{
                 int np = dp[i+1][j];
                 
                 dp[i][j] = (p+np)%mod;
+            //    if(j==0){dp[i][j]=max(dp[i][j],1);}
            }
        }
        
